7e3c683f3cf450c15d920881008e52e39a7095dd
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2024-01-29 13:43:48 +0100

    fix max 50 elements RPM

--- F1RaceApp/src/main/java/wut/f1raceapp/RaceController.java
+++ F1RaceApp/src/main/java/wut/f1raceapp/RaceController.java
@@ -111,8 +111,12 @@ public class RaceController implements Initializable, FlagObserver {
 
         rpmSeries.getData().clear();
 
-        // Add new data to the series and hide the symbols
-        for (DriverData data : dataList) {
+        // Determine the starting index to get the last 50 elements
+        int startIndex = Math.max(dataList.size() - 50, 0);
+
+        // Add only the last 50 new data to the series
+        for (int i = startIndex; i < dataList.size(); i++) {
+            DriverData data = dataList.get(i);
             XYChart.Data<String, Number> dataPoint = new XYChart.Data<>(formatDate(parseDate(data.getDate())), data.getRpm());
             rpmSeries.getData().add(dataPoint);
 
